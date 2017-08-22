a6e23a14e2cfc34b1875e978f9f739390e81c060
RiflerRick <rajdeep.mukherjee@st.niituniversity.in>
2017-08-22 10:41:51 +0530

    removed unnecessary methods

--- Github_Merge_Conflict_Intimator.py
+++ Github_Merge_Conflict_Intimator.py
@@ -9,15 +9,12 @@ The script will be taking 6 command line arguments:
 - diff filepath
 """
 
-import dateutil.parser as dtparser, datetime
 import sys
 import os
 import traceback
 import configparser
-import re
-import requests, urllib2
+import requests
 import subprocess
-from unidiff import PatchSet
 
 HEAD_BRANCH = "prodstable"
 PAT_TOKEN = os.environ["PAT_TOKEN"]
@@ -60,144 +57,6 @@ def get_conflicting_filepaths(diff_output):
         sys.exit(1)
 
     return filepaths
-
-
-def list_commits_api_call(branch_name, conflicting_filepaths, timestamp):
-    """
-    calls the github list commits api to list all commits of a particular branch, of a particular
-    file and since a particular timestamp
-
-    :param branch_name: Branch name to check for commits
-    :param conflicting_filepath: The filepath for which we need to check for commits
-    :param timestamp: The time since which we need to fetch for commits.
-    :return: a tuple containing the head_branch response list and the base_branch response list
-    """
-    # Getting the commits for the branch to which we are merging
-
-    base_branch_response = []
-    head_branch_response = []
-
-    headers = {
-        "Authorization" : "token " + PAT_TOKEN
-    }
-    for path in conflicting_filepaths:
-        params = {
-            "sha" : branch_name,
-            "path" : path,
-            "since" : str(timestamp)
-        }
-        url = os.path.join("https://api.github.com", "repos", OWNER_NAME, REPO_NAME, "commits")
-        try:
-            r = requests.get(url, headers = headers, params = params)
-            response = r.json()
-            if type(response) == list:
-                pass
-            elif type(response) == dict and response.has_key("message"):
-                raise Exception("API call for the following URL failed:\n{0}\nHeaders:\nsha:"
-                        " {1}\npath: {2}\nsince: {3}\nERROR: {4}".format(url, branch_name,
-                         path, timestamp, response["message"]))
-
-            base_branch_response.append(response)
-
-        except Exception as e:
-            exc_type, exc_val, exc_tb = sys.exc_info()
-            traceback.print_exception(exc_type, exc_val, exc_tb)
-            print str(e)
-            sys.exit(1)
-
-        try :
-            params["sha"] = HEAD_BRANCH
-            r = requests.get(url, headers=headers, params=params)
-            response = r.json()
-            if type(response) == list:
-                pass
-            elif type(response) == dict and response.has_key("message"):
-                raise Exception("API call for the following URL failed:\n{0}\nHeaders:\nsha:"
-                                " {1}\npath: {2}\nsince: {3}\nERROR: {4}".format(url, "master",
-                                 path, timestamp, response["message"]))
-
-            head_branch_response.append(response)
-
-        except Exception as e:
-            exc_type, exc_val, exc_tb = sys.exc_info()
-            traceback.print_exception(exc_type, exc_val, exc_tb)
-            print str(e)
-            sys.exit(1)
-
-    return (head_branch_response, base_branch_response)
-
-
-def parse_responses(head_branch_response, base_branch_response):
-    """
-    parses the responses to get only the information from the commits that we need.
-    In our case this information is going to be:
-    - timestamp of commit
-    - diff of the commit
-    These 2 variables will be stored in a tuple for each commit in a list of commits
-
-    :param head_branch_response:
-    :param base_branch_response:
-    :return:
-    """
-    head_branch_commits = []
-    base_branch_commits = []
-    try:
-        for response in head_branch_response:
-            for commit in response:
-                # commit is now a dictionary
-                commit_timestamp = commit["commit"]["author"]["date"]
-                # commit_diff = get_diff(commit["url"])
-                commit_diff = ""
-                commit_author_email = commit["commit"]["author"]["email"]
-                commit_author_name = commit["commit"]["author"]["name"]
-                head_branch_commits.append((commit_timestamp, commit_diff, commit_author_email,
-                                            commit_author_name))
-        for response in base_branch_response:
-            for commit in response:
-                # commit is now a dictionary
-                commit_timestamp = commit["commit"]["author"]["date"]
-                # commit_diff = get_diff(commit["url"])
-                commit_diff = ""
-                commit_author_email = commit["commit"]["author"]["email"]
-                commit_author_name = commit["commit"]["author"]["name"]
-                base_branch_commits.append((commit_timestamp, commit_diff, commit_author_email,
-                                            commit_author_name))
-
-    except Exception:
-        exc_type, exc_val, exc_tb = sys.exc_info()
-        traceback.print_exception(exc_type, exc_val, exc_tb)
-        sys.exit(1)
-
-    print "head branch commits: "
-    print head_branch_commits
-
-    print "base branch commits: "
-    print base_branch_commits
-
-    return (head_branch_commits, base_branch_commits)
-
-
-def get_all_authors(head_commits, base_commits):
-    """
-    gets all commit authors in both the base and head branch
-
-    :param head_commits: list
-    :param base_commits: list
-    :return:
-    """
-    all_authors = {
-        "head" : [],
-        "base" : []
-    }
-    for commit in head_commits:
-        t = (commit[3], commit[2])
-        all_authors["head"].append(t)
-
-    for commit in base_commits:
-        t = (commit[3], commit[2])
-        all_authors["base"].append(t)
-
-    return all_authors
 
 
 def wrap_html_anchor_tag(value, url):
@@ -540,25 +399,9 @@ for filepath in conflicting_filepaths:
     for commit in base_commits:
         base_commits_all_files.append(commit)
 
-# print "head_commits_all_files:------------------------------------"
-# print head_commits_all_files
-# print "base_commits_all_files:------------------------------------"
-# print base_commits_all_files
-# OKAY TILL HERE
 
 head_info, base_info = get_commit_authors(head_commits_all_files, base_commits_all_files)
 
-# head_branch_response, base_branch_response = list_commits_api_call(BRANCH_NAME,
-#                                                                    conflicting_filepaths,
-#                                                                    build_timestamp)
-# head_branch_commits, base_branch_commits = parse_responses(head_branch_response, base_branch_response)
-#
-# all_authors = get_all_authors(head_branch_commits, base_branch_commits)
-
-# print "head_info----------------------------------------------"
-# print head_info
-# print "base_info----------------------------------------------"
-# print base_info
 all_authors = {
         "head" : [],
         "base" : []
